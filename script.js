b1b92c93359a7e9d0e22385e3f257df24a0cbdb0
HusseinHany <hhany3997@gmail.com>
2023-03-15 20:57:29 +0200

    Implement round code

--- script.js
+++ script.js
@@ -5,3 +5,45 @@ function getComputerChoice() {
     // using mathematical equation to get a number in range of [0, n - 1]
     return choices[Math.floor(Math.random() * 3)]; // hardcoded because always constant
 }
+
+function lowerize(str){
+    return str.toLowerCase();
+}
+
+
+function game(playerSelection, computerSelection) {
+    // to be not case-sensitive, let's convert the inputs to lower case
+    lowerize(playerSelection), lowerize(computerSelection);
+
+    // handling draw case
+    if (playerSelection === computerSelection) {
+        return `Draw! player: ${playerSelection} and computer: ${computerSelection}`;
+    }
+
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return `You won! ${playerSelection} beats ${computerSelection}`
+    }
+
+    if (playerSelection === "scissors" && computerSelection === "paper") {
+        return `you won! ${playerSelection} beats ${computerSelection}`
+    }
+
+    if (playerSelection === "paper" && computerSelection === "rock") {
+        return `you won! ${playerSelection} beats ${computerSelection}`
+    }
+
+    if (playerSelection === "scissors" && computerSelection === "rock") {
+        return `you lose! ${computerSelection} beats ${playerSelection}`
+    }
+
+    if (playerSelection === "paper" && computerSelection === "scissors") {
+        return `you lose! ${computerSelection} beats ${playerSelection}`
+    }
+
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return `you lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
+
+
+// testing the code out
