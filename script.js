d6369713a18cefcbbab8dcc270f711dceb3ff3fc
HusseinHany <hhany3997@gmail.com>
2023-03-15 20:39:23 +0200

    Implement getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// console.log("hello wrold");
+// playing against computer, so randomly get a choice
+function getComputerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"];
+
+    // using mathematical equation to get a number in range of [0, n - 1]
+    return choices[Math.floor(Math.random() * 3)]; // hardcoded because always constant
+}
