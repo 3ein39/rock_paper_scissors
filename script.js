f5a0ec19fe27f95e62948c02470c0ec00b8cbf74
HusseinHany <hhany3997@gmail.com>
2023-04-25 01:03:54 +0200

    Implement some DOM buttons

--- script.js
+++ script.js
@@ -18,54 +18,75 @@ function game(playerSelection, computerSelection,result) {
 
     // handling draw case
     if (playerSelection === computerSelection) {
-        return `Draw! player: ${playerSelection} and computer: ${computerSelection}`;
+        return [`Draw! player: ${playerSelection} and computer: ${computerSelection}`, -1];
     }
 
     if (playerSelection === "rock" && computerSelection === "scissors") {
         result.num++;
-        return `You won! ${playerSelection} beats ${computerSelection}`
+        return [`You won! ${playerSelection} beats ${computerSelection}`,1]
     }
 
     if (playerSelection === "scissors" && computerSelection === "paper") {
         result.num++;
-        return `you won! ${playerSelection} beats ${computerSelection}`
+        return [`you won! ${playerSelection} beats ${computerSelection}`,1]
     }
 
     if (playerSelection === "paper" && computerSelection === "rock") {
         result.num++;
-        return `you won! ${playerSelection} beats ${computerSelection}`
+        return [`you won! ${playerSelection} beats ${computerSelection}`, 1]
     }
 
     if (playerSelection === "scissors" && computerSelection === "rock") {
-        return `you lose! ${computerSelection} beats ${playerSelection}`
+        return [`you lose! ${computerSelection} beats ${playerSelection}`, 0]
     }
 
     if (playerSelection === "paper" && computerSelection === "scissors") {
-        return `you lose! ${computerSelection} beats ${playerSelection}`
+        return [`you lose! ${computerSelection} beats ${playerSelection}`, 0]
     }
 
     if (playerSelection === "rock" && computerSelection === "paper") {
-        return `you lose! ${computerSelection} beats ${playerSelection}`
+        return [`you lose! ${computerSelection} beats ${playerSelection}`, 0]
     }
 }
 
 
 // testing the code out
-function playRound() {
-    let result = {num: 0};
-    for (let i = 1; i <= 5; ++i) {
-        let playerSelection = window.prompt("Make your choice!: ");
+let game_finished = false;
+let game_count = 0;
+let status_div = document.getElementById('status');
+let player_div = document.getElementById('player_score');
+let computer_div = document.getElementById('cmp_score');
+let result = {num: 0};
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+    if (game_finished)
+        return;
+    // let game return 1 if I won and 0 if computer one
+    // returns -1 if the game was a draw
+    let [message, won] = game(playerSelection, computerSelection, result);
+    status_div.innerText = message;
+    if (won === 1)
+        player_div.innerText++;
+    else if (won === 0)
+        computer_div.innerText++;
+    if (won !== -1)
+        ++game_count;
 
-        let computerSelection = getComputerChoice();
-        console.log(`Round :${i}`, " : " , game(playerSelection, computerSelection, result));
+    game_finished = game_count === 5;
+
+    if (game_finished) {
+        if (result.num >= 3)
+            status_div.innerText = 'congrats you won!';
+        else
+            status_div.innerText = 'humm loser!'
     }
-
-    if (result >= 3)
-        console.log("Congrats you won!\n");
-    else
-        console.log("Hmm, loser!\n")
 }
 
-playRound();
+let buttons = document.querySelectorAll('#buttons button');
+buttons.forEach((button) => {
+    // console.log(button.attributes.choice.value);
+    let choice = button.attributes.choice.value;
+    button.addEventListener('click',() => playRound(choice));
+})
 
 
